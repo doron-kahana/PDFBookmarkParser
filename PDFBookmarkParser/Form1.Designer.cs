2c2ad7d5735bd782950f3928757ab8ac2421c8b8
MoreThanSomething <doron.kahana@gmail.com>
2017-07-27 13:47:25 +0300

    First commit

---
+++ PDFBookmarkParser/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PDFBookmarkParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.readBookmarksButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(73, 88);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Browse...";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FilePath";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // readBookmarksButton
+            // 
+            this.readBookmarksButton.Location = new System.Drawing.Point(73, 177);
+            this.readBookmarksButton.Name = "readBookmarksButton";
+            this.readBookmarksButton.Size = new System.Drawing.Size(136, 23);
+            this.readBookmarksButton.TabIndex = 0;
+            this.readBookmarksButton.Text = "Read Bookmarks";
+            this.readBookmarksButton.Click += new System.EventHandler(this.readBookmarksButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(643, 255);
+            this.Controls.Add(this.readBookmarksButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button filePath;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button readBookmarksButton;
+
+    }
+}
+
---
+++ PDFBookmarkParser/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using System.Text.RegularExpressions;
+
+namespace PDFBookmarkParser
+{
+    public partial class Form1 : Form
+    {
+        private static IList<Dictionary<string, object>> bookmarks;
+        TreeView treeView = new TreeView();
+        private static PdfReader pdfReader;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        private void Init()
+        {
+            try
+            {
+                treeView.Nodes.Clear();
+                pdfReader.Close();
+            }
+            catch
+            {
+            }
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                OpenPDF(file);
+            }
+        }
+
+        private void OpenPDF(string file)
+        {
+            try
+            {
+                label2.Text = file;
+                pdfReader = new PdfReader(file);
+                bookmarks = SimpleBookmark.GetBookmark(pdfReader);
+            }
+            catch (IOException)
+            {
+            }
+        }
+
+        private void readBookmarksButton_Click(object sender, EventArgs e)
+        {
+            Init();
+            ParseBookmarks();
+        }
+
+        private void ParseBookmarks()
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+            bw.RunWorkerAsync();
+        }
+
+        public TreeNode recursive_search(IList<Dictionary<string, object>> ilist, TreeNode node)
+        {
+            int bkIndex = 0;
+
+            foreach (Dictionary<string, object> bk in ilist)
+            {
+                foreach (KeyValuePair<string, object> kvr in bk)
+                {
+                    if (kvr.Key == "Kids" || kvr.Key == "kids")
+                    {
+                        IList<Dictionary<string, object>> child = (IList<Dictionary<string, object>>)kvr.Value;
+                        recursive_search(child, node.Nodes[bkIndex]);
+                    }
+                    else if (kvr.Key == "Title" || kvr.Key == "title")
+                    {
+                        node.Nodes.Add(new TreeNode(kvr.Value.ToString()));
+                    }
+                    else if (kvr.Key == "Page" || kvr.Key == "page")
+                    {
+                        //saves page number
+                        TreeNode newNode = new TreeNode(kvr.Value.ToString());
+                        newNode.ToolTipText = Regex.Match(kvr.Value.ToString(), "[0-9]+").Value;
+                        node.Nodes.Add(newNode);
+                    }
+                }
+
+                bkIndex++;
+            }
+
+            return node;
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            TreeNode root = new TreeNode();
+
+            foreach (Dictionary<string, object> bk in bookmarks)    // top-level bookmarks
+            {
+                foreach (KeyValuePair<string, object> kvr in bk)
+                {
+                    if (kvr.Key == "Kids" || kvr.Key == "kids")
+                    {
+                        IList<Dictionary<string, object>> child = (IList<Dictionary<string, object>>)kvr.Value;
+                        treeView.Nodes.Add(recursive_search(child, root));
+                    }
+                    else if (kvr.Key == "Title" || kvr.Key == "title")
+                    {
+                        root = new TreeNode(kvr.Value.ToString());
+                    }
+                    else if (kvr.Key == "Page" || kvr.Key == "page")
+                    {
+                        //saves page number
+                        TreeNode newNode = new TreeNode(kvr.Value.ToString());
+                        newNode.ToolTipText = Regex.Match(kvr.Value.ToString(), "[0-9]+").Value;
+                        treeView.Nodes.Add(newNode);
+                    }
+                }
+            }
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                Init();
+                OpenPDF(file);
+                ParseBookmarks();
+            }
+        }
+
+    }
+}
+
